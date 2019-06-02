455206b5156e486e2a09dcef3674711f9b125442
udayk <udaykumar1113@gmail.com>
2019-06-02 07:32:58 -0400

    Displayed all types of values for datatypes

--- 2 Language basics/script.js
+++ 2 Language basics/script.js
@@ -1,1 +1,8 @@
-        console.log("First Javascript");
+var firstName='Jon';
+var lastName='Doe'
+console.log(firstName+' '+lastName);
+var age=30;
+var legalAge=true;
+console.log(legalAge);
+var defination;
+console.log(defination);
