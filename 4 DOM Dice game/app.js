7a8e31df95bd6e39b2a906104dc151e2f45efade
udayk <udaykumar1113@gmail.com>
2019-06-18 09:47:24 -0400

    Using documnet object modified HTML tag values and hided a image

--- 4 DOM Dice game/app.js
+++ 4 DOM Dice game/app.js
@@ -6,3 +6,17 @@ GAME RULES:
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
 */
+
+var scores, roundScore, activePlayer, dice;
+scores=[0,0];
+roundScore=0;
+activePlayer=0;
+
+dice=Math.floor(Math.random()*6+1);
+console.log(dice);
+
+//document.querySelector('#current-'+activePlayer).textContent=dice;
+
+document.querySelector('#current-'+activePlayer).innerHTML='<em>'+dice+'</em>';
+
+document.querySelector('.dice').style.display='none';
