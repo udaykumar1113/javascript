4fcd8818f157d26e7a0612b17b31d9e739e6ab8b
udayk <udaykumar1113@gmail.com>
2019-06-21 18:08:07 -0400

    fixed a coding error

--- 5 Advanced JS Objects and Functions/app.js
+++ 5 Advanced JS Objects and Functions/app.js
@@ -1,11 +1,12 @@
 var Person=function(name, birthYear, job){
-    this.name=name,
-    this.birtYear=birthYear,
-    this.job=job
-    this.calcAge=function(){
-        console.log(2019-birthYear);
-    }
+    this.name=name;
+    this.birthYear=birthYear;
+    this.job=job;
 }
+
+Person.prototype.calcAge=function(){
+    console.log(2019-this.birthYear);
+};
 
 var john=new Person('John',1980,'Manager');
 var max=new Person('Max',1985,'Instructor');
