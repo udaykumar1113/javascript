4fb1283e484423350f4f6afca4488841bb4331cd
udayk <udaykumar1113@gmail.com>
2019-06-21 18:27:42 -0400

    Created objects using Object.create

--- 5 Advanced JS Objects and Functions/app.js
+++ 5 Advanced JS Objects and Functions/app.js
@@ -1,4 +1,4 @@
-var Person=function(name, birthYear, job){
+/*var Person=function(name, birthYear, job){
     this.name=name;
     this.birthYear=birthYear;
     this.job=job;
@@ -15,5 +15,36 @@ console.log(john);
 console.log(max);
 
 john.calcAge();
-max.calcAge();
+max.calcAge();*/
+
+//Object.create
+var personProto={
+    calcAge:function(){
+        console.log(2019-this.birthYear);
+    }
+};
+
+var jonas=Object.create(personProto);
+jonas.name='Jonas';
+jonas.birthYear=1985;
+jonas.job='Instructor';
+
+var john=Object.create(personProto,
+{
+    name:{value: 'John'},
+    birthYear:{value: 1980},
+    job:{value:'Manager'}
+});
+
+jonas.calcAge();
+john.calcAge();
+
+
+
+
+
+
+
+
+
 
