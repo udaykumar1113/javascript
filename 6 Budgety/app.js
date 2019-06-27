0a8d3b8a4b745861ec2f32f30fb0a9bb1b4046bf
udayk <udaykumar1113@gmail.com>
2019-06-27 09:36:29 -0400

    Read input values in a module and passed them to different module

--- 6 Budgety/app.js
+++ 6 Budgety/app.js
@@ -4,17 +4,38 @@ var budgetController=(function(){
 })();
 
 var UIController=(function(){
-    //UI related code
+    
+    var DOMStrings={
+        inputType:'.add__type',
+        inputDescription:'.add__description',
+        inputValue:'.add__value',
+        inputBtn:'.add__btn'
+    }
+    
+    return{
+        getInput:function(){
+            return{
+                type: document.querySelector(DOMStrings.inputType).value,
+                description: document.querySelector(DOMStrings.inputDescription).value,
+                value: document.querySelector(DOMStrings.inputValue).value
+            };
+        },
+        
+        getDOMStrings:function(){
+            return DOMStrings;
+        }
+    };    
 })();
 
 var controller=(function(budgetCtrl, UICtrl){
 
     
-  var ctrlAddItem=function(){
-      console.log('Event listener works....')
-  }    
- document.querySelector('.add__btn').addEventListener('click',ctrlAddItem);
+  var DOMStrings=UICtrl.getDOMStrings();
     
-document.addEventListener('keypress', ctrlAddItem);
+  var ctrlAddItem=function(){
+      console.log(UICtrl.getInput());
+  }  
+  
+  document.querySelector(DOMStrings.inputBtn).addEventListener('click',ctrlAddItem);
 
 })(budgetController,UIController);
