bf1435f29e91cffdabad7e3a3209a8f356a778e2
udayk <udaykumar1113@gmail.com>
2019-07-01 18:55:39 -0400

    Inserted the user input on to UI based on income or expense type

--- 6 Budgety/app.js
+++ 6 Budgety/app.js
@@ -68,7 +68,9 @@ var UIController=(function(){
         inputType:'.add__type',
         inputDescription:'.add__description',
         inputValue:'.add__value',
-        inputBtn:'.add__btn'
+        inputBtn:'.add__btn',
+        incomeContainer:'.income__list',
+        expensesContainer:'.expenses__list'
     }
     
     return{
@@ -78,6 +80,33 @@ var UIController=(function(){
                 description: document.querySelector(DOMStrings.inputDescription).value,
                 value: document.querySelector(DOMStrings.inputValue).value
             };
+        },
+        
+        addListItem:function(obj, type){
+            
+            
+            var html, newHtml, element;
+            //Create HTML string with placeholder text
+            if(type==='inc')
+                {
+                    element=DOMStrings.incomeContainer;
+                    html='<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+                }
+            else 
+              if(type='exp')
+                {
+                    element=DOMStrings.expensesContainer;
+                    html='<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+                }
+           
+            //Replace placeholder text with actual data
+              newHtml=html.replace('%id%',obj.id);
+              newHtml=newHtml.replace('%description%',obj.description);
+              newHtml=newHtml.replace('%value%',obj.value);
+            
+            //Insert HTML into DOM
+            document.querySelector(element).insertAdjacentHTML('beforeEnd',newHtml);
+            
         },
         
         getDOMStrings:function(){
@@ -110,6 +139,9 @@ var controller=(function(budgetCtrl, UICtrl){
       var newItem=budgetCtrl.addItem(input.type, input.description, input.value);
       
       console.log(newItem);
+      
+      //3.Add the item to UI
+      UICtrl.addListItem(newItem, input.type);
       
   }
   
