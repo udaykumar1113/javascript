4cd7dc91fa0c3fbd8d9a579f11b3e72dae0c742e
udayk <udaykumar1113@gmail.com>
2019-07-10 09:42:42 -0400

    Did some destructuring of data on objects and also returned destructured objects from functions

--- 7 ES6/app.js
+++ 7 ES6/app.js
@@ -188,3 +188,41 @@ Person.prototype.myFriends6 = function(friends) {
 }
 
 new Person('Mike').myFriends6(friends);
+
+//Destructuring
+// ES5
+var john = ['John', 26];
+var name5 = john[0];
+var age5 = john[1];
+console.log(name5+' '+age5);
+
+
+// ES6
+const [name, age] = ['John', 26];
+console.log(name);
+console.log(age);
+
+const obj = {
+    firstName: 'John',
+    lastName: 'Smith'
+};
+
+const {firstName, lastName} = obj;
+console.log(firstName);
+console.log(lastName);
+
+const {firstName: a, lastName: b} = obj;
+console.log(a);
+console.log(b);
+
+
+
+function calcAgeRetirement(year) {
+    const age = new Date().getFullYear() - year;
+    return [age, 65 - age];
+}
+
+
+const [age2, retirement] = calcAgeRetirement(1990);
+console.log(age2);
+console.log(retirement);
