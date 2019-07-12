97ec9f13d7489d21180eb201edb0de4e983f49a1
udayk <udaykumar1113@gmail.com>
2019-07-12 19:01:09 -0400

    Implemented default parameters

--- 7 ES6/app.js
+++ 7 ES6/app.js
@@ -352,3 +352,29 @@ function isFullAge6(limit, ...years) {
 }
 
 isFullAge6(16, 1990, 1999, 1965, 2016, 1987);
+
+//Default parameters
+// ES5
+function SmithPerson(firstName, yearOfBirth, lastName, nationality) {
+    
+    lastName === undefined ? lastName = 'Smith' : lastName = lastName;
+    nationality === undefined ? nationality = 'american' : nationality = nationality;
+    
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.yearOfBirth = yearOfBirth;
+    this.nationality = nationality;
+}
+
+
+//ES6
+function SmithPerson(firstName, yearOfBirth, lastName = 'Smith', nationality = 'american') {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.yearOfBirth = yearOfBirth;
+    this.nationality = nationality;
+}
+
+
+var john = new SmithPerson('John', 1990);
+var emily = new SmithPerson('Emily', 1983, 'Diaz', 'spanish');
